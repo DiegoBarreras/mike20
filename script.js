0a2dc4b401a67ab7472ab14a9c9becbc9d4ebe44
DiegoBarreras <99460219+DiegoBarreras@users.noreply.github.com>
2026-03-24 01:22:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let edad = 21;
+let altura = 175;
+document.getElementById("hola").innerHTML = "<h3>Hola</h3>";
+document.write('SUMA: 21 + 175 = ', edad + altura, '<br></br>');
+document.write('RESTA: 175 - 25 = ', altura - edad, '<br></br>');
+document.write('DIVISION: 175 / 25 = ', altura / edad, '<br></br>');
+document.write('MULTIPLICACION: 175 * 25 = ', altura * edad, '<br></br>');
+document.write('EXPONENCIACION: 175 ^ 25 = ', altura ** edad, '<br></br>');
